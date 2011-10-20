3956c9ef04aef8233b7ad0887dc23b7878b7c5dd
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-10-20 12:29:23 +0200

    More explicit request initialization

--- framework1.php
+++ framework1.php
@@ -8,5 +8,13 @@ $request = Request::createFromGlobals();
 
 $request->get('foo');
 $request->query->get('foo', 'default');
+$request->query->getInt('num', 1);
 $request->request->has('foo');
 $request->files->get('foo');
+
+// ?foo[bar]=bar
+$a = $request->query->get('foo');
+$a['bar'];
+$a = $request->query->get('foo[bar]', 1, true);
+
+$request = Request::create('?foo=bar', 'GET');
