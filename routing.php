c6ecc9fce48f5f0512db628d55ff2ef04f860ed3
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-10-20 14:29:14 +0200

    Grouped routes into collections

--- routing.php
+++ routing.php
@@ -8,31 +8,32 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RequestContext;
 
 
-$routes = new RouteCollection();
-$routes->add('hello', new Route('/hello', array('name' => 'World')));
-$routes->add('hello2', new Route(
+$routes1 = new RouteCollection();
+$routes1->add('hello', new Route('/hello', array('name' => 'World')));
+$routes1->add('hello2', new Route(
     '/hello/{name}',
     array('name' => 'World'),
     array(
         'name' => '\w{3,}',
         '_method' => 'GET',
-    )
-));
-$routes->add('hello3', new Route(
-    '/hello/{name}',
-    array('name' => 'World'),
-    array(
-        '_method' => 'POST',
         '_scheme' => 'https',
     )
 ));
+$routes1->add('hello3', new Route(
+    '/hello/{name}',
+    array('name' => 'World')
+));
 
-$routes->add('dir1', new Route('/dir'));
-$routes->add('dir2', new Route('/dir/'));
+$routes2 = new RouteCollection();
+$routes2->add('dir1', new Route('/dir'));
+$routes2->add('dir2', new Route('/dir/'));
+
+$routes1->addCollection($routes2, '/ls');
 
 $context = new RequestContext('', 'POST');
 
-$matcher = new UrlMatcher($routes, $context);
+$matcher = new UrlMatcher($routes1, $context);
 
 print_r($matcher->match('/hello'));
 print_r($matcher->match('/hello/Tobias'));
+print_r($matcher->match('/ls/dir'));
