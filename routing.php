fa9248ee7fc8202ee49636bc35f8dbbd7b9e19b7
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-10-20 14:26:53 +0200

    Added requirements to routes

--- routing.php
+++ routing.php
@@ -10,15 +10,29 @@ use Symfony\Component\Routing\RequestContext;
 
 $routes = new RouteCollection();
 $routes->add('hello', new Route('/hello', array('name' => 'World')));
-$routes->add('hello2', new Route('/hello/{name}', array('name' => 'World')));
+$routes->add('hello2', new Route(
+    '/hello/{name}',
+    array('name' => 'World'),
+    array(
+        'name' => '\w{3,}',
+        '_method' => 'GET',
+    )
+));
+$routes->add('hello3', new Route(
+    '/hello/{name}',
+    array('name' => 'World'),
+    array(
+        '_method' => 'POST',
+        '_scheme' => 'https',
+    )
+));
 
-$routes->add('hello2', new Route('/dir'));
-$routes->add('hello2', new Route('/dir/'));
+$routes->add('dir1', new Route('/dir'));
+$routes->add('dir2', new Route('/dir/'));
 
-$context = new RequestContext();
+$context = new RequestContext('', 'POST');
 
 $matcher = new UrlMatcher($routes, $context);
 
 print_r($matcher->match('/hello'));
 print_r($matcher->match('/hello/Tobias'));
-print_r($matcher->match('/foor-bar'));
