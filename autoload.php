0a51b3ec47558784c533eba5c05f0f9e653e97a3
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-10-20 09:39:22 +0200

    Using namespaces for brevity

--- autoload.php
+++ autoload.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__.'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Symfony\Component\ClassLoader\MapClassLoader;
+
+$loader = new UniversalClassLoader();
 $loader->register();
 
 $loader->registerNamespace('Symfony', __DIR__.'/symfony/src');
