11d67a95b29f5c15dcfaf29ed0a5aaf42bb22bd7
Tobias Sjösten <tobias.sjosten@gmail.com>
2011-10-20 11:41:29 +0200

    Parameterized Foo class

--- servicecontainer.php
+++ servicecontainer.php
@@ -17,16 +17,29 @@ require_once __DIR__.'/autoload.php';
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Parameter;
+
+// Object description.
 
 $container = new ContainerBuilder();
 
 $container->register('bar', 'Bar');
 $container
-    ->register('foo', 'Foo')
+    ->register('foo', '%foo.class%')
     ->addArgument(new Reference('bar'))
     ->addMethodCall('init', array(new Reference('bar')))
-;
+    ;
+
+// Configuration.
+
+$container->setParameter('foo.class', 'Foo');
+
+// Testing.
 
 echo 'foo:'.spl_object_hash($container->get('foo'))."\n";
 echo 'foo:'.spl_object_hash($container->get('foo'))."\n";
 echo 'bar:'.spl_object_hash($container->get('bar'))."\n";
+
+// Usage.
+
+$container->get('foo')->drink();
